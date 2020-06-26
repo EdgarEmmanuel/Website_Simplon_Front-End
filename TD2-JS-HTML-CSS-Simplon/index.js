206e0625e74279253c69db15af2f0e4b15e39808
Edgar <mmnledgar@gmail.com>
2020-06-26 12:19:15 +0000

    TD2:fin du script js pour la page login

--- TD2-JS-HTML-CSS-Simplon/index.js
+++ TD2-JS-HTML-CSS-Simplon/index.js
@@ -1,42 +1,57 @@
-let secretaire_radio = document.querySelector("#a");
-let admin_radio = document.querySelector("#b");
-let respo_compte = document.querySelector("#c");
 
+//get all the fields
+
+let login_input = document.querySelector("#login");
+let password_input = document.querySelector("#pwd");
+let type_employe = document.querySelector("#type_employe");
 
 class UI_loginPage{
-    static Buuton_checked_treatment(){
-        
-        secretaire_radio.addEventListener("click",(e)=>{
-            //let the initial buttons to  white
-            respo_compte.parentElement.style.color='white';
-           admin_radio.parentElement.style.color='white';
-            
-            //change for the current checked
-            secretaire_radio.parentElement.style.color='#ea6a9f';
-        });
-
-        admin_radio.addEventListener("click",(e)=>{
-            //let the initial color of the others radio buttons
-            secretaire_radio.parentElement.style.color='white';
-            respo_compte.parentElement.style.color='white';
-            
-            //change for the current checked
-           admin_radio.parentElement.style.color='#ea6a9f';
-        });
-
-        respo_compte.addEventListener("click",(e)=>{
-            //let the initial color of the others radio 
-            secretaire_radio.parentElement.style.color='white';
-            admin_radio.parentElement.style.color='white';
-            
-            //change for the current checked
-            respo_compte.parentElement.style.color='#ea6a9f';
-        });
+    
+    static clearField(){
+        document.querySelector("#login").value="";
+        document.querySelector("#pwd").value="";
+        document.querySelector("#type_employe").value="";
     }
+    
+    static messageDis(mess){
+        document.querySelector("body").insertAdjacentHTML("beforeend",`<div class="divMess">
+        <h1>${mess} </h1>
+    </div>`);
+        
+        setTimeout(()=>{
+            document.querySelector("body").removeChild(document.querySelector(".divMess"));
+        },3000)
+        
+    }
+    
+    static verify_loginPage(){
+            if(login_input.value.trim()==="" || password_input.value.trim()==="" || type_employe.value.trim()===""){
+            UI_loginPage.messageDis("VEUILLEZ REMPLIR TOUS LES CHAMPS ");
+        }else{
+             console.log(login_input.value.trim());
+             console.log(password_input.value.trim());
+              console.log(type_employe.value.trim());
+            UI_loginPage.clearField();
+        }
+    }
+    
+    
+    
 }
+    
+
+//after click on the butoton connexionn
+document.querySelector("#connect").addEventListener("click",(e)=>{
+    e.preventDefault();
+    UI_loginPage.verify_loginPage();
+});
 
 
-//for the button when we checked them 
-UI_loginPage.Buuton_checked_treatment();
+//when we click on the button annuler
+document.querySelector("#annuler").addEventListener("click",(e)=>{
+    e.preventDefault();
+     UI_loginPage.clearField();
+})
+
 
 
